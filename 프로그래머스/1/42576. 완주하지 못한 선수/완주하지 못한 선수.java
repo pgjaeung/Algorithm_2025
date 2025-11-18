d37e797982bc8825b98b379d6ef058fa54c5da3b
pgjaeung <64916999+pgjaeung@users.noreply.github.com>
2025-11-18 21:02:57 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 78.63 ms, Memory: 98 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,11 +1,10 @@
 import java.util.HashMap;
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        HashMap<String, Integer> hash = new HashMap<>();
-        for(String s : participant) hash.put(s, hash.getOrDefault(s, 0) + 1);
+        HashMap<String,Integer> hash = new HashMap<>();
+        for(String s : participant) hash.put(s,hash.getOrDefault(s,0)+1);
         for(String s : completion) hash.put(s, hash.get(s)-1);
-        
         for(String s : hash.keySet()) if(hash.get(s)>0) return s;
-        return "";
+        return null;
     }
 }
